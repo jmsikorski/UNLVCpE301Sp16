f940226b1e6b9b0ed45cc20db9027ac0e28fa6fb
jmsikorski <sikorsk4@unlv.nevada.edu>
2016-02-23 16:51:25 -0800

    commented code

--- DA1/Task_a/Task_a/main.asm
+++ DA1/Task_a/Task_a/main.asm
@@ -1,18 +1,18 @@
-.INCLUDE "header.inc"
-.ORG 0x00
+.INCLUDE "header.inc"				;Header file contains macros & subroutines
+.ORG 0x00							;Begining of code
 
-	INITSTACK
+	INITSTACK						;Initialize the stack pointer
 
-	LDI		ZL, LOW(Var_Address)
+	LDI		ZL, LOW(Var_Address)	;Load Z with variable address
 	LDI		ZH, HIGH(Var_Address)
-	LDI		TEMP1, LOW(RAMEND/2)
-	LDI		COUNT, 25
+	LDI		TEMP1, LOW(RAMEND/2)	;Load Temp1 with initial value 
+	LDI		COUNT, 25				;Number of values to add
 Get25:
-	ST		Z+, TEMP1
-	SUBI	TEMP1, -4
-	DEC		COUNT
-	CPI		COUNT, 0
+	ST		Z+, TEMP1				;Store Temp1 into (Z) then increment Z
+	SUBI	TEMP1, -4				;Update Temp1 value
+	DEC		COUNT					;Decrement Counter
+	CPI		COUNT, 0				;Loop until Counter equals 0
 	BRNE	Get25
 
 End1:
-	RJMP	End1
+	RJMP	End1					;Infinite loop
--- DA1/Task_d/Task_d/main.asm
+++ DA1/Task_d/Task_d/main.asm
@@ -1,36 +1,38 @@
-.INCLUDE "header.inc"
-.ORG 0x00
-.EQU	Var_Address = 0x100;
-.EQU	Num = 25
+.INCLUDE "header.inc"				;Header file contains macros & subroutines
+.ORG 0x00							;Begining of code
+.EQU	Var_Address = 0x100;		;Variable for values array pointer
+.EQU	Num = 25					;Variable for number of values
 	INITSTACK
 
-	LDI		ZL, LOW(Var_Address)
+	LDI		ZL, LOW(Var_Address)	;Load Z with variable array address
 	LDI		ZH, HIGH(Var_Address)
-	LDI		TEMP1, LOW(RAMEND/2)
-	LDI		COUNT, Num
+	LDI		TEMP1, LOW(RAMEND/2)	;Load Temp1 with initial value 
+	LDI		COUNT, Num				;Number of values to add
+	
 GetNum:
-	ST		Z+, TEMP1
-	SUBI	TEMP1, -4
-	DEC		COUNT
-	CPI		COUNT, 0
+	ST		Z+, TEMP1				;Store Temp1 into (Z) then increment Z
+	SUBI	TEMP1, -4				;Update Temp1 value
+	DEC		COUNT					;Decrement Counter
+	CPI		COUNT, 0				;Loop until Counter equals 0
 	BRNE	GetNum
 
-	LDI		ZL, LOW(Var_Address)
+	LDI		ZL, LOW(Var_Address)	;Load Z with array index[0]
 	LDI		ZH, HIGH(Var_Address)
-	LDI		COUNT, Num
+	LDI		COUNT, Num				;Variable Counter
 Divide:
-	LD		TEMP1, Z
-	CALL	Div7
-	LD		TEMP1, Z+
-	CALL	Div3
-	DEC		COUNT
-	CPI		COUNT, 0
+	LD		TEMP1, Z				;Load TEMP1 with the value in (Z), post increment Z
+	CALL	Div7					;Call Div7 Subroutine
+	LD		TEMP1, Z+				;Reload TEMP1
+	CALL	Div3					;Call Div3 Subroutine
+	DEC		COUNT					;Decrement Counter
+	CPI		COUNT, 0				;Loop until Counter equals 0
 	BRNE	Divide
 	
-	CPI		R24, 0
-	BREQ	End1
-	LDI		TEMP1, 8
-	OR		R7, TEMP1
+	ANDI	R7, 0					;Clear R7
+	CPI		R24, 0					;Check if R24 has a value (i.e. R23 has overflowed)
+	BREQ	End1					;If R23 hasn't overflown, jump to end
+	LDI		TEMP1, 8				;Set bit 3 high
+	OR		R7, TEMP1				
 
 End1:
-	RJMP	End1
+	RJMP	End1					;Infinite Loop
